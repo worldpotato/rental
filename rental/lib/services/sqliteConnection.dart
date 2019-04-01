62f064879c2d0b2057aa7e4c85f00c031ac638dc
Worldpotato <fstrobel@hm.edu>
2019-04-02 00:53:47 +0200

    Opens Database in sqliteConnection
    Signed-off-by: Worldpotato <fstrobel@hm.edu>

--- rental/lib/services/sqliteConnection.dart
+++ rental/lib/services/sqliteConnection.dart
@@ -1,7 +1,29 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 
 class SqliteConnection{
 
-
-
+  Database db;
+  //open a new database connection
+  Future open() async {
+    //specify a path where the database should be stored
+    var databasePath = await getDatabasesPath(); //system specific! (automatically)
+    var path = join(databasePath, 'rental.db');
+    //try to open the database, if it does not exist create it
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute(
+              '''
+            create table items ( 
+              itemId integer primary key, 
+              itemName text not null,
+              itemDescription text not null,
+              itemStatus text not null,
+              itemOwner text not null)
+          '''
+          ); //just as a note $variableName is the same like "String " + variableName.toString() + " other String"
+        }
+    );
+  }
 }

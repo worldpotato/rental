d94e7e5981d73f6545b0f1a64037e28ad8c6307e
Worldpotato <fstrobel@hm.edu>
2019-04-02 01:08:36 +0200

    Use SqliteConnection in dbWrapper to open a sqlite DB
    Signed-off-by: Worldpotato <fstrobel@hm.edu>

--- rental/lib/services/dataBaseConnection.dart
+++ rental/lib/services/dataBaseConnection.dart
@@ -1,3 +1,15 @@
+import 'package:rental/services/sqliteConnection.dart';
+
+/// Wrapper class for a connection to a data base.
+/// For example a sqlite data base.
 class DataBaseConnection {
 
+  SqliteConnection sqliteConnection;
+  DataBaseConnection(){
+    sqliteConnection = SqliteConnection();
+  }
+
+  Future open() async {
+    sqliteConnection.open();
+  }
 }
